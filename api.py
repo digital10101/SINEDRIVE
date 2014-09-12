c00f5d741d477577b7fc65c92fc145595dd4730c
Parv Rustogi <Parv@Parvs-MacBook-Air.local>
2014-09-13 00:18:12 +0530

    most of the changes

--- api.py
+++ api.py
@@ -123,6 +123,261 @@ class UpdateRead(restful.Resource):
         return data
 
 
+class Follow(restful.Resource):
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_id', type=int, required=True)
+        parser.add_argument('band_id', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.follow(request_params['user_id'], request_params['band_id'])
+
+        return data
+
+
+class UsersFollowing(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_id', type=int, required=True)
+        parser.add_argument('band_id', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.users_following(request_params['user_id'], request_params['msg_id'])
+
+        return data
+
+
+class GetSines(restful.Resource):
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('seq_no', type=int, required=True)
+        parser.add_argument('band_id', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.get_sines(request_params['seq_no'], request_params['band_id'])
+
+        return data
+
+
+class AddPlayTimes(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_id', type=int, required=True)
+        parser.add_argument('song_id', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.users_following(request_params['user_id'], request_params['song_id'])
+
+        return data
+
+
+class Collaboration(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_id', type=int, required=True)
+        parser.add_argument('sender', type=int, required=True)
+        parser.add_argument('message', type=str, required=True)
+        parser.add_argument('audio', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.collab(request_params['user_id'], request_params['sender'], request_params['message'], request_params['audio'])
+
+        return data
+
+
+class CollaborationRequest(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_id', type=int, required=True)
+        parser.add_argument('requested_to', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.collab_req(request_params['user_id'], request_params['requested_to'])
+
+        return data
+
+
+class CollaborationAccept(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_id', type=int, required=True)
+        parser.add_argument('requested_to', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.collab_accept(request_params['user_id'], request_params['requested_to'])
+
+        return data
+
+
+class GetQuestion(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('wall_user_id', type=int, required=True)
+        parser.add_argument('seq_no', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.get_question(request_params['wall_user_id'], request_params['seq_no'])
+
+        return data
+
+class PostQuestion(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_id', type=int, required=True)
+        parser.add_argument('wall_user_id', type=int, required=True)
+        parser.add_argument('question', type=str, required=True)
+        request_params = parser.parse_args()
+
+        data = util.post_question(request_params['user_id'], request_params['wall_user_id'], request_param['question'])
+
+        return data
+
+
+class GetComment(restful.Resource):
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('ques_id', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.get_comment(request_params['ques_id'])
+
+        return data
+
+
+class GetAnswered(restful.Resource):
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('ques_id', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.get_answered(request_params['ques_id'])
+
+        return data
+
+
+class GetUnanswered(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('ques_id', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.get_unanswered(request_params['ques_id'])
+
+        return data
+
+
+class PostAnswer(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('ques_id', type=int, required=True)
+        parser.add_argument('answer', type=str, required=True)
+        request_params = parser.parse_args()
+
+        data = util.post_answer(request_params['ques_id'], request_params['answer'])
+
+        return data
+
+
+class PostComment(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('ques_id', type=int, required=True)
+        parser.add_argument('user_id', type=str, required=True)
+        parser.add_argument('comment', type=str, required=True)
+        request_params = parser.parse_args()
+
+        data = util.post_comment(request_params['ques_id'],request_params['user_id'], request_params['comment'])
+
+        return data
+
+
+class DeleteAnswer(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('ques_id', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.delete_answer(request_params['ques_id'])
+
+        return data
+
+
+class DeleteQuestion(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('ques_id', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.delete_question(request_params['ques_id'])
+
+        return data
+
+
+class DeleteComment(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('msg_id', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.delete_comment(request_params['msg_id'])
+
+        return data
+
+
+class DeleteInboxMessage(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('msg_id', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.delete_inbox_message(request_params['msg_id'])
+
+        return data
+
+
+class Search(restful.Resource):
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('search_term', type=int, required=True)
+        parser.add_argument('query_filter', type=str, required=True)
+        request_params = parser.parse_args()
+
+        data = util.search(request_params['search_term'], request_params['query_filter'])
+
+        return data
+
+
+class GetNotification(restful.Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_id', type=int, required=True)
+        parser.add_argument('seq_no', type=int, required=True)
+        request_params = parser.parse_args()
+
+        data = util.get_notif(request_params['user_id'], request_params['seq_no'])
+
+        return data
+
 
 api.add_resource(GetNewTrack, '/get_next_track')
 api.add_resource(Signup, '/signup')
@@ -131,6 +386,26 @@ api.add_resource(UserSongsData, '/user_songs_data')
 api.add_resource(SongVote, '/like_dislike')
 api.add_resource(Mail, '/mail')
 api.add_resource(UpdateRead, '/update_read')
+api.add_resource(Follow, '/follow')
+api.add_resource(UsersFollowing, '/users_following')
+api.add_resource(GetSines, '/get_sines')
+api.add_resource(AddPlayTimes, '/add_play_times')
+api.add_resource(Collaboration, '/collab')
+api.add_resource(CollaborationRequest, '/collab_req')
+api.add_resource(CollaborationAccept, '/collab_accept')
+api.add_resource(GetQuestion, '/get_question')
+api.add_resource(PostQuestion, '/post_question')
+api.add_resource(GetComment, '/get_comment')
+api.add_resource(GetAnswered, '/get_answered')
+api.add_resource(GetUnanswered, '/get_unanswered')
+api.add_resource(PostAnswer, '/post_answer')
+api.add_resource(PostComment, '/post_comment')
+api.add_resource(DeleteAnswer, '/delete_answer')
+api.add_resource(DeleteQuestion, '/delete_question')
+api.add_resource(DeleteComment, '/delete_comment')
+api.add_resource(DeleteInboxMessage, '/delete_inbox_message')
+api.add_resource(Search, '/search')
+api.add_resource(GetNotification, '/get_notif')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True, use_reloader=True)
--- util.py
+++ util.py
@@ -1,13 +1,14 @@
 __author__ = 'karan'
 from mysql_wrapper import Mysql
+import MySQLdb
 from operator import itemgetter
 import random
 import ujson
 
 
-def get_user_song_data(user_id, offset):
+def get_user_song_data(user_id, offset, genre):
 
-    songs = songs_g_values(offset)
+    songs = songs_g_values(offset, genre)
     songs_rel = get_songs_related_to(user_id)
 
     song_g_ids = list()
@@ -39,16 +40,27 @@ def get_user_song_data(user_id, offset):
     return {'error': 0, 'data': songs_g}
 
 
-def songs_g_values(offset):
+def songs_g_values(offset, genre):
     query = 'Select song_filename, album, song_id, title, band_name from songs_info order by g_value desc limit 20 offset %s' % str((offset - 1) * 20)
-    mysql = Mysql()
-    rows = ()
-    try:
-        rows = mysql.getManyRows(query)
-    except Exception as e:
-        print e
-    mysql.close()
-    print rows
+    query1 = 'Select song_filename, album, song_id, title, band_name from songs_info where genre = %s order by g_value desc limit 20 offset %s' % (genre, str((offset - 1) * 20))
+    if genre == 'show all':
+        mysql = Mysql()
+        rows = ()
+        try:
+            rows = mysql.getManyRows(query)
+        except Exception as e:
+            print e
+        mysql.close()
+        print rows
+    else:
+        mysql = Mysql()
+        rows = ()
+        try:
+            rows = mysql.getManyRows(query1)
+        except Exception as e:
+            print e
+        mysql.close()
+        print rows
     return list(rows)
 
 
@@ -64,6 +76,7 @@ def get_songs_related_to(user_id):
     mysql.close()
     rows = list(rows)
     return rows
+
 
 def get_next_likes(user_id, song_id):
 
@@ -120,10 +133,10 @@ def song_vote(user_id, song_id, liked, disliked):
     mysql.close()
 
     g_value = (row['numl'] - row['numd']) * 2 + row['numpt']
-    query = 'update songs_info set g_value = %d where song_id = %d' %(g_value, song_id)
+    query = 'update songs_info set g_value = %d where song_id = %d'
     mysql = Mysql()
     try:
-        mysql.execute(query)
+        mysql.execute(query, (g_value, song_id))
     except Exception as e:
         print e
     mysql.close()
@@ -132,9 +145,27 @@ def song_vote(user_id, song_id, liked, disliked):
 
 def mail(rec_id, sender_id, message, sub):
     mysql = Mysql()
-    query = 'insert into `inbox` (rec_id, sender_id, message, subject) values(%d, %d, "%s", "%s")' % (rec_id, sender_id, mysql.escape_string(message), mysql.escape_string(sub))
+    query = 'insert into `inbox` (rec_id, sender_id, message, subject) values(%d, %d, "%s", "%s")'
     try:
-        mysql.execute(query)
+        mysql.execute(query, (rec_id, sender_id, mysql.escape_string(message), mysql.escape_string(sub)))
+    except Exception as e:
+        print e
+    mysql.close()
+
+    msg_query = 'select msg_id from inbox where rec_id = %d and sender_id = %d and message = "%s" and subject = "%s"' % (rec_id, sender_id, mysql.escape_string(message), mysql.escape_string(sub))
+    rows = {}
+    mysql = Mysql()
+    try:
+        rows = mysql.getSingleRow(msg_query)
+    except Exception as e:
+        print e
+    mysql.close()
+    msg_id = rows['msg_id']
+
+    query = 'insert into `notification` (type_id, msg_id, created_by, for_id, viewed) values (102, %d, %d, %d, 0)'
+    mysql = Mysql()
+    try:
+        mysql.execute(query, (msg_id, sender_id, rec_id))
     except Exception as e:
         print e
     mysql.close()
@@ -143,7 +174,7 @@ def mail(rec_id, sender_id, message, sub):
 
 def subject(rec_id, seq_no):
 
-    query = 'Select login.name , inbox.msg_id , inbox.subject , inbox.read , inbox.timestamp from inbox join login on inbox.sender_id = login.user_id where inbox.rec_id = %d order by inbox.timestamp desc limit 2 offset %s' % (rec_id, str((seq_no - 1) * 2))
+    query = 'Select login.name , inbox.msg_id , inbox.subject , inbox.read , inbox.timestamp from inbox join login on inbox.sender_id = login.user_id where inbox.rec_id = %d order by inbox.timestamp desc limit 10 offset %s' % (rec_id, str((seq_no - 1) * 10))
     mysql = Mysql()
     rows = None
     try:
@@ -177,7 +208,495 @@ def update_read(msg_id):
     return {'error': 0}
 
 
+def follow(band_id):
+
+    query = 'select count(user_id) from follow_data where band_id = %d' % band_id
+    mysql = Mysql()
+    row = {}
+    try:
+        row = mysql.getSingleRow(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    return row
 
 
+def users_following(user_id, band_id):
 
+    query = 'insert into `follow_data` (user_id, band_id) values(%d, %d)' % (user_id, band_id)
+    mysql = Mysql()
+    try:
+        mysql.execute(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    return {'error': 0}
+
+
+def get_sines(seq_no, band_id):
+
+    query = 'select * from songs_info where band_id = %d order by Timestamp desc limit 10 offset %s' % (band_id, str((seq_no - 1) * 10))
+    mysql = Mysql()
+    rows = None
+    try:
+        rows = mysql.getManyRows(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    rows = list(rows)
+    msg_dict = dict()
+    for row in rows:
+        row['timestamp'] = row['timestamp'].isoformat()
+        msg_dict[str(row['msg_id'])] = row
+
+    response = {
+        'data': msg_dict,
+        'error': 0
+    }
+
+    return response
+
+
+def add_played_times(user_id, song_id):
+
+    query = 'insert into `user_songs` (played_times) values (0) on duplicate key update played_times = (played_times + 1) where user_id = %d and song_id = %d' % (user_id, song_id)
+    mysql = Mysql()
+    try:
+        mysql.execute(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    return {'error': 0}
+
+
+def collab(user_id, sender, message, audio):
+
+    query = 'insert into collab_info (user_id, sender, message, audio) values (%d, %d, "%s", %d)'
+    mysql = Mysql()
+    try:
+        mysql.execute(query, (user_id, sender, message, audio))
+    except Exception as e:
+        print e
+    mysql.close()
+
+    msg_query = 'select msg_id from collab_info where user_id = %d and sender = %d and message = "%s" and audio = %d' % (user_id, sender, mysql.escape_string(message), audio)
+    rows = {}
+    mysql = Mysql()
+    try:
+        rows = mysql.getSingleRow(msg_query)
+    except Exception as e:
+        print e
+    mysql.close()
+    msg_id = rows['msg_id']
+
+    query = 'insert into `notification` (type_id, msg_id, created_by, for_id, viewed) values (101, %d, %d, %d, 0)'
+    mysql = Mysql()
+    try:
+        mysql.execute(query, (msg_id, sender, user_id))
+    except Exception as e:
+        print e
+    mysql.close()
+
+    return {'error': 0}
+
+
+def collab_req(user_id, requested_to):
+
+    query = 'insert into `collab_request` (user_id, requested_to) values (%d, %d)' % (user_id, requested_to)
+    mysql = Mysql()
+    try:
+        mysql.execute(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    return {'error': 0}
+
+
+def collab_accept(user_id, requested_to):
+
+    query = 'update collab_request set accepted = 1 where user_id = %d and requested_to = %d' % (user_id, requested_to)
+    mysql = Mysql()
+    try:
+        mysql.execute(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    return {'error': 0}
+
+
+def get_question(wall_user_id, seq_no):
+
+    query = 'select * from questions where wall_user_id = %d order by Timestamp desc limit 10 offset %s' % (wall_user_id, str((seq_no - 1) * 10))
+    mysql = Mysql()
+    rows = None
+    try:
+        rows = mysql.getManyRows(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    rows = list(rows)
+    ques_dict = dict()
+    for row in rows:
+        row['timestamp'] = row['timestamp'].isoformat()
+        ques_dict[str(row['ques_id'])] = row
+
+    response = {
+        'data': ques_dict,
+        'error': 0
+    }
+
+    # query1 = 'update questions set read = 1 where wall_user_id = %d' % wall_user_id
+    # mysql = Mysql()
+    # try:
+    #     mysql.execute(query1)
+    # except Exception as e:
+    #     print e
+    # mysql.close()
+
+    return response
+
+
+def post_question(question, user_id, wall_user_id):
+
+    query = 'insert into `questions` (question, user_id, wall_user_id) values (%s, %d, %d)' % (question, user_id, wall_user_id)
+    mysql = Mysql()
+    try:
+        mysql.execute(query)
+    except Exception as e:
+        print e
+    mysql.close()
+
+    msg_query = 'select ques_id from questions where question = "%s" and user_id = %d and wall_user_id = %d' % (question, user_id, wall_user_id)
+    rows = {}
+    mysql = Mysql()
+    try:
+        rows = mysql.getSingleRow(msg_query)
+    except Exception as e:
+        print e
+    mysql.close()
+    msg_id = rows['ques_id']
+
+    query = 'insert into `notification` (type_id, msg_id, created_by, for_id, viewed) values (103, %d, %d, %d, 0)'
+    mysql = Mysql()
+    try:
+        mysql.execute(query, (msg_id, user_id, wall_user_id))
+    except Exception as e:
+        print e
+    mysql.close()
+
+    return {'error': 0}
+
+
+def get_comment(user_id):
+
+    query = 'select * from comments where user_id = %d' % user_id
+    mysql = Mysql()
+    rows = None
+    try:
+        rows = mysql.getManyRows(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    rows = list(rows)
+    comment_dict = dict()
+    for row in rows:
+        row['timestamp'] = row['timestamp'].isoformat()
+        comment_dict[str(row['msg_id'])] = row
+
+    response = {
+        'data': comment_dict,
+        'error': 0
+    }
+
+    return response
+
+
+def get_answered(ques_id):
+
+    query = 'select ques_id, question, user_id, wall_user_id, timestamp from questions where ques_id = %d and answer_flag = 1' % ques_id
+    mysql = Mysql()
+    rows = None
+    try:
+        rows = mysql.getManyRows(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    rows = list(rows)
+    ans_dict = dict()
+    for row in rows:
+        row['timestamp'] = row['timestamp'].isoformat()
+        ans_dict[str(row['ques_id'])] = row
+
+    response = {
+        'data': ans_dict,
+        'error': 0
+    }
+
+    return response
+
+
+def get_unanswered(ques_id):
+
+    query = 'select ques_id, question, user_id, wall_user_id, timestamp from questions where ques_id = %d and answer_flag = 0' % ques_id
+    mysql = Mysql()
+    rows = None
+    try:
+        rows = mysql.getManyRows(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    rows = list(rows)
+    unans_dict = dict()
+    for row in rows:
+        row['timestamp'] = row['timestamp'].isoformat()
+        unans_dict[str(row['ques_id'])] = row
+
+    response = {
+        'data': unans_dict,
+        'error': 0
+    }
+
+    return response
+
+
+def post_answer(user_id, answer):
+
+    query = 'update questions set answer_flag = 1 and answer = %s were user_id = %d' % (answer, user_id)
+    mysql = Mysql()
+    try:
+        mysql.execute(query)
+    except Exception as e:
+        print e
+    mysql.close()
+
+    msg_query = 'select ques_id, wall_user_id from questions where answer = "%s" and user_id = %d' % (answer, user_id)
+    rows = {}
+    mysql = Mysql()
+    try:
+        rows = mysql.getSingleRow(msg_query)
+    except Exception as e:
+        print e
+    mysql.close()
+    msg_id = rows['ques_id']
+    wall_id = rows['wall_user_id']
+
+    query = 'insert into `notification` (type_id, msg_id, created_by, for_id, viewed) values (104, %d, %d, %d, 0)'
+    mysql = Mysql()
+    try:
+        mysql.execute(query, (msg_id, wall_id, user_id))
+    except Exception as e:
+        print e
+    mysql.close()
+
+    return {'error': 0}
+
+
+def post_comment(ques_id, user_id, comment):
+
+    query = 'update comments set user_id = case when ques_id = %d then %d else user_id end, comment = case when ques_id = %d then %s else comment end' % (ques_id, user_id, ques_id, comment)
+    mysql = Mysql()
+    try:
+        mysql.execute(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    return {'error': 0}
+
+
+def delete_answer(ques_id):
+
+    query = 'update questions set answer = null, answer_flag = 0 where ques_id = %d' % ques_id
+    mysql = Mysql()
+    try:
+        mysql.execute(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    return {'error': 0}
+
+
+def delete_question(ques_id):
+
+    query = 'delete from question where ques_id = %d' % ques_id
+    mysql = Mysql()
+    try:
+        mysql.execute(query)
+    except Exception as e:
+        print e
+    mysql.close()
+
+    query = 'delete from comments where ques_id = %d' % ques_id
+    mysql = Mysql()
+    try:
+        mysql.execute(query)
+    except Exception as e:
+        print e
+    mysql.close()
+
+    return {'error': 0}
+
+
+def delete_comment(msg_id):
+
+    query = 'delete from comments where msg_id = %d' % msg_id
+    mysql = Mysql()
+    try:
+        mysql.execute(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    return {'error': 0}
+
+
+def delete_inbox_message(msg_id):
+
+    query = 'delete from inbox where msg_id = %d' % msg_id
+    mysql = Mysql()
+    try:
+        mysql.execute(query)
+    except Exception as e:
+        print e
+    mysql.close()
+    return {'error': 0}
+
+
+count = 0
+final_result = list()
+
+
+def srch_song(term):
+
+    global final_result
+    global count
+
+    query = 'select * from songs_info where title like %%%d%% LIMIT 15'
+    rows = None
+    mysql = Mysql()
+    try:
+        rows = mysql.getManyRows(query, term)
+    except Exception as e:
+        print e
+    mysql.close()
+    if len(query) > 0:
+        final_result.append(rows)
+        return len(rows)
+    else:
+        return 0
+
+
+def srch_album(term):
+
+    global final_result
+    global count
+
+    query = 'select album_filename, album, band_name, band_id , count(album)from songs_info where album like %%%d%% group by album LIMIT 15'
+    rows = None
+    mysql = Mysql()
+    try:
+        rows = mysql.getManyRows(query, term)
+    except Exception as e:
+        print e
+    mysql.close()
+    if len(query) > 0:
+        final_result.append(rows)
+        return len(rows)
+    else:
+        return 0
+
+
+def srch_artist(term):
+
+    global final_result
+    global count
+
+    query = 'select band_name, band_id, login.profile_pic from songs_info join login on login.user_id = songs_info.band_id where album like %%%d%% LIMIT 15'
+    rows = None
+    mysql = Mysql()
+    try:
+        rows = mysql.getManyRows(query, term)
+    except Exception as e:
+        print e
+    mysql.close()
+    if len(query) > 0:
+        final_result.append(rows)
+        return len(rows)
+    else:
+        return 0
+
+
+def srch_person(term):
+
+    global final_result
+    global count
+
+    query = 'select band_name, band_id, login.profile_pic from songs_info join login on login.user_id = songs_info.band_id where album like %%%d%% LIMIT 15'
+    rows = None
+    mysql = Mysql()
+    try:
+        rows = mysql.getManyRows(query, term)
+    except Exception as e:
+        print e
+    mysql.close()
+    if len(query) > 0:
+        final_result.append(rows)
+        return len(rows)
+    else:
+        return 0
+
+
+def search(search_term, query_filter):
+
+    terms = search_term.split(' ')
+    search_len = 15
+    limit = min(search_len, len(terms))
+    global count
+    global final_result
+    final_result = 0
+
+    if query_filter == 'all':
+        for term in terms[:(limit - 1)]:
+            songs = srch_song(term)
+            count += songs
+            if count < 15:
+                album = srch_album(term)
+                count += album
+                if count < 15:
+                    artist = srch_artist(term)
+                    count += artist
+    elif query_filter == 'song':
+        for term in terms[:(limit - 1)]:
+            songs = srch_song(term)
+            count += songs
+    elif query_filter == 'album':
+        for term in terms[:(limit - 1)]:
+            album = srch_album(term)
+            count += album
+    elif query_filter == 'artist':
+        for term in terms[:(limit - 1)]:
+            artist = srch_artist(term)
+            count += artist
+
+    return final_result
+
+
+def get_notif(user_id, seq_no):
+
+    query = 'select * from notification where viewed = 0 and for_id = %d order by Timestamp desc limit 15 offset %s'
+    mysql = Mysql()
+    rows = None
+    try:
+        rows = mysql.getManyRows(query, (user_id, str((seq_no - 1) * 10)))
+    except Exception as e:
+        print e
+    mysql.close()
+
+    query = 'update notification set viewed = 1 where viewed = 0 and for_id = %d'
+    mysql = Mysql()
+    try:
+        mysql.execute(query, user_id)
+    except Exception as e:
+        print e
+    mysql.close()
+
+    return rows
 
